23e690134c488e7bd8480e449b7287ffdb8f6833
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-08-10 18:24:21 +0300

    Add CORS & fix docker files

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 from backend.json_encoder import OrderJSONEncoder
 from common.database import Session
 from common.database.models.order import Order
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.from_prefixed_env()
 app.json_encoder = OrderJSONEncoder
 
