3b5fda0d0b9e8bc949ce6c670e1331ef5a0940a7
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-08-10 17:30:50 +0300

    Add DB interaction to the backend

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
+from backend.json_encoder import OrderJSONEncoder
+from common.database import Session
+from common.database.models.order import Order
+
 app = Flask(__name__)
+app.config.from_prefixed_env()
+app.json_encoder = OrderJSONEncoder
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-    return "Hello World!"
+    session = Session()
+    return session.query(Order).all()
+
---
+++ backend/json_encoder.py
@@ -0,0 +1,18 @@
+import time
+from flask.json import JSONEncoder
+
+from common.database.models.order import Order
+from common.database.utils import model_to_dict
+
+
+class OrderJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Order):
+            data = model_to_dict(obj)
+            data |= {
+                'date': time.mktime(obj.date.timetuple()),
+                'cost_dollars': float(obj.cost_dollars),
+                'cost_rubles': float(obj.cost_rubles),
+            }
+            return data
+        return super().default(obj)
